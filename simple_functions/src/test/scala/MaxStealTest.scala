7d933a92cac6c027f31d0b3e8916abb7336afdfa
Jon Inloes <326619+jinloes@users.noreply.github.com>
2015-06-23 19:37:42 -0700

    added max steal problem

---
+++ simple_functions/src/test/scala/MaxStealTest.scala
@@ -0,0 +1,36 @@
+import com.jinloes.simple_functions.MaxSteal
+import org.scalatest.{FeatureSpec, GivenWhenThen, Matchers}
+
+/**
+ * Tests for {@link MaxSteal}
+ */
+class MaxStealTest extends FeatureSpec with Matchers with GivenWhenThen {
+  feature("A thief should be able to steal the max values from a houses") {
+    scenario("A thief should steal from 1 house") {
+      Given("one house")
+      val arr = Array(3)
+      When("the thief steals")
+      val max = MaxSteal.steal(arr)
+      Then("The max amount the thief can steal is 3")
+      max should be(3)
+    }
+
+    scenario("A thief should steal from 2 houses") {
+      Given("one house")
+      val arr = Array(3, 5)
+      When("the thief steals")
+      val max = MaxSteal.steal(arr)
+      Then("The max amount the thief can steal is 5")
+      max should be(5)
+    }
+
+    scenario("A thief should steal from 4 houses") {
+      Given("one house")
+      val arr = Array(6, 1, 2, 7)
+      When("the thief steals")
+      val max = MaxSteal.steal(arr)
+      Then("The max amount the thief can steal is 13")
+      max should be(13)
+    }
+  }
+}
