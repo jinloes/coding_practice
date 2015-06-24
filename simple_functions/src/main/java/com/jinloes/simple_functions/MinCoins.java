451238205801606e6b2ff931e559aad19ad51b8a
Jon Inloes <326619+jinloes@users.noreply.github.com>
2015-06-23 18:52:53 -0700

    cleaned up min coins example

--- simple_functions/src/main/java/com/jinloes/simple_functions/MinCoins.java
+++ simple_functions/src/main/java/com/jinloes/simple_functions/MinCoins.java
@@ -10,9 +10,9 @@ public class MinCoins {
     public static int findMinCoins(int sum, int[] coinValues) {
         Map<Integer, Integer> minCoins = new HashMap<>();
         minCoins.put(0, 0);
-        for(int i = 1; i <= sum; i++) {
+        for (int i = 1; i <= sum; i++) {
             int closestCoin = findClosestCoin(i, coinValues);
-            if(i - closestCoin > 0) {
+            if (i - closestCoin > 0) {
                 minCoins.put(i, minCoins.get(closestCoin) + minCoins.get(i - closestCoin));
             } else {
                 minCoins.put(i, 1);
@@ -23,8 +23,8 @@ public class MinCoins {
 
     private static int findClosestCoin(int value, int[] coinValues) {
         int closestCoin = 0;
-        for(int i = 0; i < coinValues.length; i++) {
-            if(value - coinValues[i] >= 0) {
+        for (int i = 0; i < coinValues.length; i++) {
+            if (value - coinValues[i] >= 0) {
                 closestCoin = coinValues[i];
             }
         }
@@ -34,12 +34,14 @@ public class MinCoins {
     public static int findMinCoinsAlternative(int sum, int[] coinValues) {
         Map<Integer, Integer> sumMap = new HashMap<>();
         sumMap.put(0, 0);
-        for(int i =0; i < sum; i++) {
-            for(int j = 0; j < coinValues.length; j++) {
-                int foundSum = i + coinValues[j];
+        // Until we reach the sum
+        for (int i = 0; i <= sum; i++) {
+            // add each coin value to previously known sum and see if it takes less coins to achieve than currently
+            // known
+            for (int value : coinValues) {
+                int foundSum = i + value;
                 int numCoins = sumMap.get(i) + 1;
-                if(!sumMap.containsKey(foundSum)
-                        || sumMap.get(foundSum) > numCoins) {
+                if (!sumMap.containsKey(foundSum) || sumMap.get(foundSum) > numCoins) {
                     sumMap.put(foundSum, numCoins);
                 }
             }
