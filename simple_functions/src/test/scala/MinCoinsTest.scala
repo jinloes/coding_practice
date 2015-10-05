be511c39fcc14723d35272b99f6017a119ff00e4
Jon Inloes <326619+jinloes@users.noreply.github.com>
2015-10-05 11:38:18 -0700

    bug fixing

--- simple_functions/src/test/scala/MinCoinsTest.scala
+++ simple_functions/src/test/scala/MinCoinsTest.scala
@@ -18,16 +18,17 @@ class MinCoinsTest extends FlatSpec with Matchers {
   }
 
   "Find min coins alternative" should "return the minimum number of coins equal a sum" in {
-    MinCoins.findMinCoinsAlternative(0, Array(1, 3, 5)) should be (0)
-    MinCoins.findMinCoinsAlternative(1, Array(1, 3, 5)) should be (1)
-    MinCoins.findMinCoinsAlternative(3, Array(1, 3, 5)) should be (1)
-    MinCoins.findMinCoinsAlternative(5, Array(1, 3, 5)) should be (1)
-    MinCoins.findMinCoinsAlternative(4, Array(1, 3, 5)) should be (2)
-    MinCoins.findMinCoinsAlternative(11, Array(1, 3, 5)) should be (3)
-    MinCoins.findMinCoinsAlternative(15, Array(1, 3, 5)) should be (3)
-    MinCoins.findMinCoinsAlternative(21, Array(1, 3, 5)) should be (5)
+    MinCoins.findMinCoinsAlternative(0, Array(1, 3, 5)) should be(0)
+    MinCoins.findMinCoinsAlternative(1, Array(1, 3, 5)) should be(1)
+    MinCoins.findMinCoinsAlternative(3, Array(1, 3, 5)) should be(1)
+    MinCoins.findMinCoinsAlternative(5, Array(1, 3, 5)) should be(1)
+    MinCoins.findMinCoinsAlternative(4, Array(1, 3, 5)) should be(2)
+    MinCoins.findMinCoinsAlternative(11, Array(1, 3, 5)) should be(3)
+    MinCoins.findMinCoinsAlternative(15, Array(1, 3, 5)) should be(3)
+    MinCoins.findMinCoinsAlternative(21, Array(1, 3, 5)) should be(5)
 
-    MinCoins.findMinCoins(16, Array(1, 2, 4, 6)) should be (3)
-    MinCoins.findMinCoins(35, Array(1, 2, 4, 6)) should be (7)
+    MinCoins.findMinCoinsAlternative(16, Array(1, 2, 4, 6)) should be(3)
+    MinCoins.findMinCoinsAlternative(35, Array(1, 2, 4, 6)) should be(7)
+    MinCoins.findMinCoinsAlternative(1, Array(2, 3)) should be(0)
   }
 }
