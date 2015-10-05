86f82477b3fb5e9ce8e3f57d2f8692a41ac254d7
Jon Inloes <326619+jinloes@users.noreply.github.com>
2015-10-05 15:47:42 -0700

    adding a failing test
    Change-Id: Ic1140c4f1feade1e638ec31de56446cca2e54d1b

---
+++ event_handler/src/test/java/com/jinloes/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.jinloes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by jinloes on 10/5/15.
+ */
+public class ExampleTest {
+    @Test
+    public void test() {
+        //Assert.fail();
+    }
+}
