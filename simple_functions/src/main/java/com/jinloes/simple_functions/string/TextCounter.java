f7a044a618c2dddae782c5dfffb9b4e0b98978aa
Jon Inloes <326619+jinloes@users.noreply.github.com>
2021-01-21 12:55:08 -0800

    Added text counter

---
+++ simple_functions/src/main/java/com/jinloes/simple_functions/string/TextCounter.java
@@ -0,0 +1,52 @@
+package com.jinloes.simple_functions.string;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * Takes in a paragraph as a string and returns a report of count of the words sorted by word length and then the words
+ * lexicographically.
+ * <p>
+ * ie ab ab -> 2 ab
+ */
+public class TextCounter {
+    private static final Pattern NON_WORD_CHAR = Pattern.compile("[^a-zA-Z1-9]$");
+
+    public List<String> count(String text) {
+        if (text == null || text.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        StringTokenizer tokenizer = new StringTokenizer(text);
+
+        Comparator<String> textCounterComparator = (o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            } else {
+                return o1.length() - o2.length();
+            }
+        };
+
+        Map<String, Integer> countMap = new TreeMap<>(textCounterComparator);
+
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            token = NON_WORD_CHAR.matcher(token).replaceAll("");
+            System.out.println(token);
+            if (countMap.containsKey(token)) {
+                int count = countMap.get(token);
+                countMap.put(token, count + 1);
+            } else {
+                countMap.put(token, 1);
+            }
+        }
+
+        System.out.println(countMap);
+
+        return countMap.entrySet()
+                .stream()
+                .map(entry -> entry.getValue() + " " + entry.getKey())
+                .collect(Collectors.toList());
+    }
+}
---
+++ simple_functions/src/test/java/com/jinloes/simple_functions/string/TextCounterTest.java
@@ -0,0 +1,48 @@
+package com.jinloes.simple_functions.string;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TextCounterTest {
+    private TextCounter textCounter;
+
+    @Before
+    public void setUp() throws Exception {
+        textCounter = new TextCounter();
+    }
+
+    @Test
+    public void testNull() {
+        assertThat(textCounter.count(null)).isEmpty();
+    }
+
+    @Test
+    public void testBlank() {
+        assertThat(textCounter.count("    ")).isEmpty();
+        assertThat(textCounter.count("\n")).isEmpty();
+        assertThat(textCounter.count("\n \r")).isEmpty();
+    }
+
+    @Test
+    public void testCountSimple() {
+        assertThat(textCounter.count("ab ab"))
+                .containsExactly("2 ab");
+
+
+    }
+
+    @Test
+    public void testQuickBrownFox() {
+        assertThat(textCounter.count("The quick brown fox jumps over the lazy dog."))
+                .contains("1 The", "1 dog", "1 fox", "1 the", "1 lazy", "1 over", "1 brown", "1 jumps", "1 quick");
+    }
+
+    @Test
+    public void testParagraph() {
+        assertThat(textCounter.count("How are you doing today? \n \r What are you doing!"))
+                .contains("1 How", "2 are", "2 you", "1 What", "2 doing", "1 today");
+    }
+
+}
